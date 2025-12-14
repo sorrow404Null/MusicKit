bfbb9703b84ade8c9a73052e227e0f0cc61fb963
sorrow404Null <GladyshHamiltons57@gmail.com>
2025-12-14 08:50:41 +0800

    feat: update app icon and misc changes

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,12 +1,7 @@
+@file:Suppress("UnstableApiUsage")
 pluginManagement {
     repositories {
-        google {
-            content {
-                includeGroupByRegex("com\\.android.*")
-                includeGroupByRegex("com\\.google.*")
-                includeGroupByRegex("androidx.*")
-            }
-        }
+        google()
         mavenCentral()
         gradlePluginPortal()
     }
