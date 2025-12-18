329850f370eb5432d3d27da905dea6a562da7183
sorrow404Null <GladyshHamiltons57@gmail.com>
2025-12-18 21:29:21 +0800

    feat(utils): add `safeDisplayName` extension for `DocumentFile`

--- app/src/main/java/com/eclipse/music/kit/utils/MiscUtils.kt
+++ app/src/main/java/com/eclipse/music/kit/utils/MiscUtils.kt
@@ -2,6 +2,7 @@ package com.eclipse.music.kit.utils
 
 import android.content.Intent
 import androidx.core.net.toUri
+import androidx.documentfile.provider.DocumentFile
 import com.eclipse.music.kit.utils.AndroidAppContext.applicationContext
 
 object MiscUtils {
@@ -11,4 +12,11 @@ object MiscUtils {
         }
         applicationContext?.startActivity(intent)
     }
+
+    fun DocumentFile.safeDisplayName(): String = name
+        ?.removeSuffix(".ncm")
+        ?.let { if (it.length > 40) it.take(35) + "..." else it }
+        ?: "Unknown"
+
+
 }
