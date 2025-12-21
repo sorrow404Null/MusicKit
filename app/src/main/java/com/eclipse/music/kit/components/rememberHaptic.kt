585f52ebb10a15a39b88a9cd6dee6e1b5deb0f9f
sorrow404Null <GladyshHamiltons57@gmail.com>
2025-12-21 16:55:35 +0800

    feat: implement `rememberHaptic` utility for managing haptic feedback levels

---
+++ app/src/main/java/com/eclipse/music/kit/components/rememberHaptic.kt
@@ -0,0 +1,40 @@
+package com.eclipse.music.kit.components
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.hapticfeedback.HapticFeedbackType
+import androidx.compose.ui.platform.LocalHapticFeedback
+import com.eclipse.music.kit.utils.data.SettingsState
+
+enum class HapticLevel {
+    LIGHT,
+    MEDIUM,
+    SUCCESS
+}
+
+@Composable
+fun rememberHaptic(
+    settings: SettingsState
+): (HapticLevel) -> Unit {
+
+    val haptic = LocalHapticFeedback.current
+
+    return remember(settings.hapticFeedbackEnabled) {
+        { level ->
+            if (!settings.hapticFeedbackEnabled) return@remember
+
+            val type = when (level) {
+                HapticLevel.LIGHT ->
+                    HapticFeedbackType.TextHandleMove
+
+                HapticLevel.MEDIUM ->
+                    HapticFeedbackType.LongPress
+
+                HapticLevel.SUCCESS ->
+                    HapticFeedbackType.Confirm
+            }
+
+            haptic.performHapticFeedback(type)
+        }
+    }
+}
